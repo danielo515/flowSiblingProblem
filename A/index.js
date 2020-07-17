8ee630ad14cdd018c94d5a2bf29084687d6e6e24
Danielo Rodriguez <rdanielo@gmail.com>
2020-07-17 18:27:18 +0200

    first commit

---
+++ A/index.js
@@ -0,0 +1,8 @@
+//@flow
+type Props = {
+  a: number,
+  b: number,
+}
+export default function A(props: Props) {
+   return props.a + props.b
+}
---
+++ B/index.js
@@ -0,0 +1,4 @@
+//@flow
+import A from 'A'
+
+console.log(A({a:1, b:[33]}))
